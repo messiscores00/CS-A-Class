b5257a6997ee150dcba3980a30b0463b8fde2cdd
Ryan Sheehy <Messiscores00@gmail.com>
2018-08-22 18:21:33 -0700

    Start of Project

--- Quadratic_Formula.java
+++ Quadratic_Formula.java
@@ -1,33 +1,26 @@
-
-/**
- * Write a description of class Quadratic_Formula here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import java.util.Scanner;
 public class Quadratic_Formula
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Quadratic_Formula
-     */
-    public Quadratic_Formula()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static void main(String[] arg){
+        /*
+         * Promt user for quadratic formula
+         * Begin parsing user expression for a,b,c coefficients
+         *     a. locate index of 1st X
+         *     b. extract substring from user expression from beginning to position of 1st X
+         *     c. Convert Substring to number form and store to variable.
+         *     d. locate index of 2nd X
+         *     e. extract substring from 1st X index + 2 to 2nd X.
+         *     f. Convert Substring to number form and store to variable.
+         *     g. extract substring from 2nd X to end
+         *     h. Convert Substring to number form and store to variable.
+         * Put variables into quadratic equation
+         * Print out.
+         */
+        System.out.println("Please enter your quadratic formula: ");
+        Scanner in = new Scanner(System.in);//new = new object using the Scanner constructor that takes in an InputStream
+        //System.in is a reference to that input stream
+        String exp = in.nextLine();//nextline scans the input screen and returns the curent line as a string
+        
+        
     }
 }
