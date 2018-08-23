517ef9e4982cdbfac140f5d50761a99f34798e6f
Ryan Sheehy <Messiscores00@gmail.com>
2018-08-23 09:24:53 -0700

    8-23-18

--- Quadratic_Formula.java
+++ Quadratic_Formula.java
@@ -11,10 +11,13 @@ public class Quadratic_Formula
          * 1. Promt user for quadratic formula and take in the user's input
          * 2. Begin parsing user expression for a,b,c coefficients
          *     a. locate index of 1st X
-         *     b. extract substring from user expression from beginning to position of 1st X and Convert Substring to number form and store to variable.
+         *     b. extract substring from user expression from beginning to position of 1st X 
+         *     c. Convert Substring to number form and store to variable.
          *     c. reset String to everything after 1st X
          *     d. locate index of 2nd X
-         *     e. extract substring from user expression from beginning to position of 2nd X and Convert Substring to number form and store to variable.
+         *     e. extract substring from user expression from beginning to position of 2nd X
+         *     f. Convert Substring to number form and store to variable.
+         *     
          *     g. reset String to everything after 2nd X
          *     h. extract substring from user expression from beginning to end and Convert Substring to number form and store to variable.
          * 3. Put variables into quadratic equation
@@ -31,28 +34,33 @@ public class Quadratic_Formula
         String exp = in.nextLine();
         //nextline scans the input screen and returns the curent line as a string
         
-        //2
-        parsingforABC(exp);
+        int firstXPos = exp.indexOf("x");
+         
+        String aStr = exp.substring(0 , firstXPos);
+        
+        double aCoeff = Double.parseDouble(aStr);
+        
+        exp = exp.substring(firstXPos + 3);
+        
+        int secondXPos = exp.indexOf("x");
+        
+        String bStr = exp.substring(0 , secondXPos);
+        
+        double bCoeff = Double.parseDouble(bStr);
+        
+        exp = exp.substring(secondXPos + 1);
+        
+        double cCoeff = Double.parseDouble(exp);
+        
         //3
-        if(b*b - 4 * a * c < 0){
+        if(bCoeff*bCoeff - 4 * aCoeff * cCoeff < 0){
             System.out.println("You answer is an imaginary number");
         }else{
-            double X1 = (- b + Math.sqrt(b*b - 4 * a * c))/ 2 * a;
-            double X2 = (- b - Math.sqrt(b*b - 4 * a * c))/ 2 * a;
+            double X1 = (- bCoeff + Math.sqrt(bCoeff*bCoeff - 4 * aCoeff * cCoeff))/ 2 * aCoeff;
+            double X2 = (- bCoeff - Math.sqrt(bCoeff*bCoeff - 4 * aCoeff * cCoeff))/ 2 * aCoeff;
          //4
             System.out.println("X1: " + X1);
             System.out.println("X2: " + X2);
         }
-        }
-    //2
-    public static void parsingforABC(String qf){
-        String quadform = qf;
-        int firstX = quadform.indexOf("x");
-        a = Double.parseDouble(quadform.substring(0, firstX));
-        quadform = quadform.substring(firstX + 2, quadform.length()+1);
-        int secondX = quadform.indexOf("x");
-        b = Double.parseDouble(quadform.substring(0, secondX));
-        quadform = quadform.substring(secondX , quadform.length()+1);
-        c = Double.parseDouble(quadform.substring(0));
     }
-}
+   }
