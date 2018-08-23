a3e99e6b486a6f8522bcbabc32e26f2f410928e4
Ryan Sheehy <Messiscores00@gmail.com>
2018-08-22 18:20:49 -0700

    Initial sharing of project

---
+++ Quadratic_Formula.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Quadratic_Formula here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Quadratic_Formula
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Quadratic_Formula
+     */
+    public Quadratic_Formula()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
